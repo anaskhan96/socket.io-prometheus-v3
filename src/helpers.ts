79a44d3c57ff2aeab27f7a8c1e4b0a64175bf40a
Anas Khan <anas.khan96@outlook.com>
2021-02-12 02:45:36 +0530

    fuck it

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,10 +1,6 @@
 const Helpers = {
   dataToBytes(data: any): number {
-    try {
-      return new TextEncoder().encode(JSON.stringify(data)).length
-    } catch (err) {
-      return 0
-    }
+    return 0
   }
 }
 
